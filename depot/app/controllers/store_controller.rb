9b0d0420dd3bc5e13cd9d5768d7a09d80fdd585d
cxbird <msdn@qq.com>
2013-10-14 22:36:51 +0800

    cart & line item show counts...

--- depot/app/controllers/store_controller.rb
+++ depot/app/controllers/store_controller.rb
@@ -1,5 +1,15 @@
 class StoreController < ApplicationController
   def index
 		@products = Product.all
+		visit_index_count
   end
+
+	private
+	def visit_index_count
+		if session[:counter].nil?
+			session[:counter]=1
+		else
+			session[:counter]+=1
+		end
+	end
 end
