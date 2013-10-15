b9b72781b0b680be86e7b54330df2303032a2d1c
cxbird <msdn@qq.com>
2013-10-15 22:32:03 +0800

    数据合并操作migrate...

--- depot/db/schema.rb
+++ depot/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131015065934) do
+ActiveRecord::Schema.define(version: 20131015092851) do
 
   create_table "carts", force: true do |t|
     t.datetime "created_at"
