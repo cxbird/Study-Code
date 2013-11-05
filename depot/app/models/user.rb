b2f24f3ee04fc5ad199f736fec8c02ba57a834cc
cxbird <msdn@qq.com>
2013-11-05 16:15:03 +0800

    Repair user ...

--- depot/app/models/user.rb
+++ depot/app/models/user.rb
@@ -1,9 +1,11 @@
+require 'digest/sha2'
+
 class User < ActiveRecord::Base
 	validates :name, :presence => true, :uniqueness => true
 	validates :password, :confirmation => true
 	attr_accessor :password_confirmation
 	attr_reader :password
-	validates :password_must_be_present
+	validate :password_must_be_present
 
 	def User.authenticate(name, password)
 		if user = find_by_name(name)
@@ -14,11 +16,11 @@ class User < ActiveRecord::Base
 	end
 
 	def User.encrypt_password(password, salt)
-		Digest::SHA2.hexdigest(pssword + "wibble" + salt)
+		Digest::SHA2.hexdigest(password + "wibble" + salt)
 	end
 
 	#'password' is virtual attribute
-	def password = (password)
+	def password=(password)
 		@password = password
 
 		if password.present?
@@ -30,7 +32,7 @@ class User < ActiveRecord::Base
 	private
 
 		def password_must_be_present
-			errors.add(:password, "Missing password") unless hashed_password.present?
+			errors.add(:password, "Missing password") unless password.present?
 		end
 
 		def generate_salt
