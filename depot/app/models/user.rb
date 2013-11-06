fb148fd76aa839d0d265429febe420446c403418
cxbird <msdn@qq.com>
2013-11-06 17:47:32 +0800

    repair user

--- depot/app/models/user.rb
+++ depot/app/models/user.rb
@@ -1,32 +1,29 @@
-require 'digest/sha2'
+#require 'digest/sha2'
 
 class User < ActiveRecord::Base
 	validates :name, :presence => true, :uniqueness => true
-	validates :password, :confirmation => true
-	attr_accessor :password_confirmation
-	attr_reader :password
+	#validates :password, :confirmation => true
+	#attr_accessor :password_confirmation
+	#attr_reader :password
 	validate :password_must_be_present
 
-	def User.authenticate(name, password)
-		if user = find_by_name(name)
-			if user.hashed_password == encrypt_password(password, user.salt)
-				user
-			end
+	def self.authenticate(name, password)
+		user = find_by_name(name)
+		if user && Digest::SHA256.hexdigest(password + user.salt)  == user.hashed_password
+			return user
 		end
-	end
-
-	def User.encrypt_password(password, salt)
-		Digest::SHA2.hexdigest(password + "wibble" + salt)
+    false
 	end
 
 	#'password' is virtual attribute
-	def password=(password)
-		@password = password
+  def password
+    @password
+  end
 
-		if password.present?
-			generate_salt
-			self.hashed_password = self.class.encrypt_password(password, salt)
-		end
+	def password=(pass)
+    return unless pass
+		@password = pass
+    generate_password(pass)
 	end
 
 	private
@@ -35,7 +32,9 @@ class User < ActiveRecord::Base
 			errors.add(:password, "Missing password") unless hashed_password.present?
 		end
 
-		def generate_salt
-			self.salt = self.object_id.to_s + rand.to_s
-		end
+		def generate_password(pass)
+		  salt = Array.new(10){rand(1024).to_s(36)}.join
+      self.salt, self.hashed_password = 
+        salt, Digest::SHA256.hexdigest(pass + salt)
+    end
 end
