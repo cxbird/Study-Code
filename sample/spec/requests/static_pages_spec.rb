2360c7ec3acaa16bf4d886691855b7a063d8c3f9
cxbird <msdn@qq.com>
2014-01-10 23:44:58 +0800

    Rspec Tool from support ...

--- sample/spec/requests/static_pages_spec.rb
+++ sample/spec/requests/static_pages_spec.rb
@@ -6,7 +6,7 @@ describe "Static Pages" do
     before { visit root_path }
 
     it { should have_content('Sample App') }
-		it { should have_title("Ruby on Rails Tutorial Sample App") }
+		it { should have_title(full_title("")) }
 		it { should_not have_title('| Home') }
   end
 
